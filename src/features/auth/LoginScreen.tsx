26a87b00bef0126a3236fa3fc61b1209d83ece65
Adnan-Ahmed07 <masudrana52443@gmail.com>
2024-12-19 14:16:11 -0700

    Landing page was Complete

---
+++ src/features/auth/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const LoginScreen=()=>  {
+  
+    return (
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    )
+  
+}
+
+export default LoginScreen
